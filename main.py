c4efa23c514e86cce6aec754103cfa23f0a735e6
Arseny <ars.kalinichenko@gmail.com>
2019-05-02 13:57:07 +0600

    add example

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello")
+import telebot
+from telebot.types import Message
+
+import configs
+
+bot = telebot.TeleBot(configs.bot_token)
+
+
+# TODO: сделать регу
+@bot.message_handler(commands=['start'])
+def welcome(message: Message):
+    bot.send_message(
+        text="Привет, я помогу тебе отслеживать скидки на книги!\n"
+             "Чтобы добавить книгу, напиши:\n`добавить [ссылка на книгу]`",
+
+        chat_id=message.from_user.id, parse_mode='Markdown')
+
+
+@bot.message_handler(func=lambda message: True)
+def upper(message: Message):
+    bot.reply_to(message, "Посмотри аниме, а не трать время впустую!".upper())
+
+
+bot.polling()
