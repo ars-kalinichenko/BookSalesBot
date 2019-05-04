96c906c18c660bfca4f4ba466b63131ac4692f66
Arseny <ars.kalinichenko@gmail.com>
2019-05-04 22:09:21 +0600

    sx

--- parser/chitai_gorod.py
+++ parser/chitai_gorod.py
@@ -1,48 +1,48 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-def get_html(url):
-    r = requests.get(url)
-    return r.text
-
-
-def get_price(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    prices = soup.select('div.product__price')
-    prices = prices[0].get_text()
-    return [int(s) for s in prices.split() if s.isdigit()][0]
-
-
-def get_title(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    title = soup.find(class_='product__header')
-    title = title.find_all('h1')
-    title = title[0].get_text()
-    return title.strip()
-
-
-def get_image_link(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    img = soup.select("div.product__image img[src]")
-    return img[0]["src"]
-
-
-def count_pages(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    title = soup.find(class_='product__properties')
-    title = title.find_all(class_='product-prop__value')[6]
-    title = title.get_text()
-    return [int(s) for s in title.split() if s.isdigit()][0]
-
-
-def main():
-    base_url = 'https://www.chitai-gorod.ru/catalog/book/1187950/?watch_fromlist=cat_9666'
-    print(get_title(get_html(base_url)))
-    print(get_image_link(get_html(base_url)))
-    print(get_price(get_html(base_url)))
-    print(count_pages(get_html(base_url)))
-
-
-if __name__ == '__main__':
-    main()
+# import requests
+# from bs4 import BeautifulSoup
+#
+#
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def get_price(html):
+#     soup = BeautifulSoup(html, 'html.parser')
+#     prices = soup.select('div.product__price')
+#     prices = prices[0].get_text()
+#     return [int(s) for s in prices.split() if s.isdigit()][0]
+#
+#
+# def get_title(html):
+#     soup = BeautifulSoup(html, 'html.parser')
+#     title = soup.find(class_='product__header')
+#     title = title.find_all('h1')
+#     title = title[0].get_text()
+#     return title.strip()
+#
+#
+# def get_image_link(html):
+#     soup = BeautifulSoup(html, 'html.parser')
+#     img = soup.select("div.product__image img[src]")
+#     return img[0]["src"]
+#
+#
+# def count_pages(html):
+#     soup = BeautifulSoup(html, 'html.parser')
+#     title = soup.find(class_='product__properties')
+#     title = title.find_all(class_='product-prop__value')[6]
+#     title = title.get_text()
+#     return [int(s) for s in title.split() if s.isdigit()][0]
+#
+#
+# def main():
+#     base_url = 'https://www.chitai-gorod.ru/catalog/book/1187950/?watch_fromlist=cat_9666'
+#     print(get_title(get_html(base_url)))
+#     print(get_image_link(get_html(base_url)))
+#     print(get_price(get_html(base_url)))
+#     print(count_pages(get_html(base_url)))
+#
+#
+# if __name__ == '__main__':
+#     main()
--- parser/labirint.py
+++ parser/labirint.py
@@ -1,48 +1,48 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-def get_html(url):
-    r = requests.get(url)
-    return r.text
-
-
-def get_price(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    prices = soup.select('div.buying-price')
-    prices = prices[0].get_text()
-    return [int(s) for s in prices.split() if s.isdigit()][0]
-
-
-def get_title(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    title = soup.find(class_='prodtitle')
-    title = title.find_all('h1')
-    title = title[0].get_text()
-    return title.split(':')[1].strip()
-
-
-def get_image_link(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    img = soup.find("div", {"id": "product-image"})
-    return img.select("img[src]")[0]['data-src']
-
-
-def count_pages(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    title = soup.find(class_='pages2')
-    title = title.get_text()
-    return [int(s) for s in title.split() if s.isdigit()][0]
-
-
-def main():
-    base_url = 'https://www.labirint.ru/books/589212/'
-
-    print(get_title(get_html(base_url)))
-    print(get_image_link(get_html(base_url)))
-    print(get_price(get_html(base_url)))
-    print(count_pages(get_html(base_url)))
-
-
-if __name__ == '__main__':
-    main()
+# import requests
+# from bs4 import BeautifulSoup
+#
+#
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def get_price(html):
+#     soup = BeautifulSoup(html, 'html.parser')
+#     prices = soup.select('div.buying-price')
+#     prices = prices[0].get_text()
+#     return [int(s) for s in prices.split() if s.isdigit()][0]
+#
+#
+# def get_title(html):
+#     soup = BeautifulSoup(html, 'html.parser')
+#     title = soup.find(class_='prodtitle')
+#     title = title.find_all('h1')
+#     title = title[0].get_text()
+#     return title.split(':')[1].strip()
+#
+#
+# def get_image_link(html):
+#     soup = BeautifulSoup(html, 'html.parser')
+#     img = soup.find("div", {"id": "product-image"})
+#     return img.select("img[src]")[0]['data-src']
+#
+#
+# def count_pages(html):
+#     soup = BeautifulSoup(html, 'html.parser')
+#     title = soup.find(class_='pages2')
+#     title = title.get_text()
+#     return [int(s) for s in title.split() if s.isdigit()][0]
+#
+#
+# def main():
+#     base_url = 'https://www.labirint.ru/books/589212/'
+#
+#     print(get_title(get_html(base_url)))
+#     print(get_image_link(get_html(base_url)))
+#     print(get_price(get_html(base_url)))
+#     print(count_pages(get_html(base_url)))
+#
+#
+# if __name__ == '__main__':
+#     main()
