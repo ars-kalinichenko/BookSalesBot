5a6756ec7bc6f1ebbfe59cac8a2087d4fb3fff9f
Arseny <ars.kalinichenko@gmail.com>
2019-05-05 16:34:29 +0600

    log remove to terminal

--- logger.py
+++ logger.py
@@ -6,6 +6,6 @@ from telebot.types import Message
 def push_msg_to_cout(message: Message):
     logging.basicConfig(format='%(asctime)s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
-                        level=logging.NOTSET)
+                        level=logging.INFO)
 
     logging.info('{} sent "{}"'.format(message.from_user.username, message.text))
