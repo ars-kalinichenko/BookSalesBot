634565425d6824c598b7da057c70653f9d333153
Arseny <ars.kalinichenko@gmail.com>
2019-05-05 15:22:13 +0600

    test

--- logger.py
+++ logger.py
@@ -8,6 +8,6 @@ def push_msg_to_log(message: Message):
                         filemode='w',
                         format='%(asctime)s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
-                        level=logging.INFO)
+                        level=logging.NOTSET)
 
     logging.info('{} sent "{}"'.format(message.from_user.username, message.text))
