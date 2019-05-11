37f7675709bc615cfd2e9d8ff59a1e19ddb91943
Arseny <ars.kalinichenko@gmail.com>
2019-05-11 13:17:38 +0600

    test

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
+import os
+
 import telebot
 from telebot.types import Message
-import os
-import logger
+
 import IOC
+import logger
 from parsers import parser_manager
 
 bot = telebot.TeleBot(os.environ.get("token"))
@@ -23,8 +25,9 @@ def reply(message: Message):
     if "добавить" in message.text.lower():
         IOC.queue_url.put(message.text.split(' ')[-1])
         bot.send_message(message.chat.id, "Хмм")
-        book = parser_manager.add_book('https://www.chitai-gorod.ru/catalog/book/1188153/?watch_fromlist=cat_9072')
-        bot.send_message(message.chat.id, book['price'])
+        book = parser_manager.add_book(IOC.queue_url[0])
+        bot.send_message(message.chat.id,
+                         f"Вы уверены,что хотите добавить книгу {book['title']} за {book['price']} рублей?")
     else:
         bot.send_message(message.chat.id, "Бот временно не работает. Приносим извинения за доставленные неудобства.")
     logger.show_msg(message)
