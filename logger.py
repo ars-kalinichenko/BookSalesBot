84f86b1a2239c2f1aa4817d6a398d268988d2bb0
Arseny <ars.kalinichenko@gmail.com>
2019-05-19 17:49:11 +0600

    Fixed bug in logger(None username)

--- logger.py
+++ logger.py
@@ -7,10 +7,10 @@ def show_msg(message: Message):
     logging.basicConfig(format='%(asctime)s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
                         level=logging.INFO)
-    try:
-        logging.info('{} sent "{}"'.format(message.from_user.username, message.text))
-    except TypeError:
+    if message.from_user.username is None:
         logging.info('{} sent "{}"'.format("Anonymous", message.text))
+    else:
+        logging.info('{} sent "{}"'.format(message.from_user.username, message.text))
 
 
 def show_error(**kwargs):
